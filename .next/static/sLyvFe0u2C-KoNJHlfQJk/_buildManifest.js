4fad89fffe872750827129786ac297c9551a69e3
abraham <abraham.turnsek@gmail.com>
2023-05-30 09:58:13 +0200

    version 1.1 (cambio conversor divisa)

---
+++ .next/static/sLyvFe0u2C-KoNJHlfQJk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/93-739c06fc0e5c1571.js","static/css/58a54bcc468db22f.css","static/chunks/pages/index-1c622264dde505be.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
